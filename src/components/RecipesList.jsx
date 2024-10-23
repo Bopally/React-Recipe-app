c678b874855a02c3ef25360ce4388586b4d2b92d
Bopally <estelleb0707@gmail.com>
2024-10-23 16:19:02 +0200

    Add a Delete Button

--- src/components/RecipesList.jsx
+++ src/components/RecipesList.jsx
@@ -1,15 +1,33 @@
 import recipes from "../data/recipes.json";
+import { useState } from "react";
 
 const RecipesList = () => {
+  const [recipesToDisplay, setrecipesToDisplay] = useState(recipes);
+
+  const deleteRecipe = (recipeId) => {
+    const newArray = recipesToDisplay.filter((recipeObj) => {
+      return recipeObj.id !== recipeId;
+    });
+    setrecipesToDisplay(newArray);
+  };
+
   return (
     <section>
-      {recipes.map((recipeObj) => {
+      {recipesToDisplay.map((recipeObj) => {
         return (
           <div key={recipeObj.id} className="Content">
             <img src={recipeObj.image} />
             <p>Name of recipe: {recipeObj.name}</p>
             <p>Calories: {recipeObj.calories}</p>
             <p>Servings: {recipeObj.servings}</p>
+            {recipeObj.calories < 200 && <p>Low calories</p>}
+            <button
+              onClick={() => {
+                deleteRecipe(recipeObj.id);
+              }}
+            >
+              Delete this recipe
+            </button>
           </div>
         );
       })}
