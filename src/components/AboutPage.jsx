019facfe5115224962d8a79c28dadac66e0bb76d
Bopally <estelleb0707@gmail.com>
2024-10-29 18:03:12 +0100

    Add style and images of team members for our About page

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -5,14 +5,14 @@ const AboutPage = () => {
       name: "Estelle",
       linkedIn: "https://www.linkedin.com/in/ballot-estelle/",
       github: "https://github.com/Bopally",
-      // image:
+      image: "images/Estelle-Image.png",
     },
     {
       id: 2,
       name: "Mariana",
       linkedIn: "https://www.linkedin.com/in/mariana-luzia-frazao/",
       github: "https://github.com/marianafrazao",
-      // image:
+      image: "images/Mariana-Image.png",
     },
   ];
 
@@ -20,15 +20,21 @@ const AboutPage = () => {
     <>
       <h1>About our project</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis
-        earum, unde, fugiat sed, alias repellat aliquid excepturi quos cumque
-        libero totam. Vero, asperiores vel.
+        {`Grandmother's recipes is a user-friendly recipe website designed to
+        inspire culinary creativity and provide a platform for cooking
+        enthusiasts to explore and share diverse recipes from around the world.
+        Whether you're a seasoned chef or a kitchen newbie, our site offers a
+        treasure trove of step-by-step recipes with stunning visuals, helpful
+        tips, and user reviews. You can check your favorite recipes, submit your
+        own culinary creations, and join a vibrant community of food lovers.
+        Dive into a world of flavors and bring your culinary dreams to life with
+        Grandmother's recipes!`}
       </p>
 
       <h2>About the team members:</h2>
       {users.map((user) => (
-        <div key={user.id}>
-          {/* <img src={user.image} alt={`${user.name}'s profile`} /> */}
+        <div key={user.id} className="team-member-box">
+          <img src={user.image} alt={`${user.name}'s profile`} />
           <p>Name: {user.name}</p>
           <p>
             LinkedIn:{" "}
